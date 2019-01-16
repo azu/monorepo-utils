3f23328984538608b411f128fc9b25b042212f5e
azu <azuciao@gmail.com>
2019-01-16 23:01:57 +0900

    chore(collect-changelog): add snapshot test

--- packages/@monorepo-utils/collect-changelog/test/cli-test.ts
+++ packages/@monorepo-utils/collect-changelog/test/cli-test.ts
@@ -2,27 +2,24 @@ import * as path from "path";
 import { execute } from "../src/cli";
 
 const fixturesDir = path.join(__dirname, "fixtures");
-describe("Snapshot testing", () => {
+describe("cli", () => {
     it("textlint@10.2.0 should has changelog ", async () => {
-        const fixtureDir = path.join(fixturesDir, "example");
         const actual = await execute({
-            directory: fixtureDir,
+            directory: fixturesDir,
             lernaTag: "textlint@10.2.0"
         });
         expect(actual).toMatchSnapshot();
     });
     it("@textlint/kernel@2.0.0-next. should has changelog ", async () => {
-        const fixtureDir = path.join(fixturesDir, "example");
         const actual = await execute({
-            directory: fixtureDir,
+            directory: fixturesDir,
             lernaTag: "@textlint/kernel@2.0.0-next.0"
         });
         expect(actual).toMatchSnapshot();
     });
     it("@textlint/ast-tester@2.0.7 should be empty ", async () => {
-        const fixtureDir = path.join(fixturesDir, "example");
         const actual = await execute({
-            directory: fixtureDir,
+            directory: fixturesDir,
             lernaTag: "@textlint/ast-tester@2.0.7"
         });
         expect(actual).toBe("");

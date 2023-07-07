135185276e4fdd49b0cd0b94108ffaa090e33c90
azu <azuciao@gmail.com>
2023-07-07 21:21:51 +0900

    fix: fix incorrect result about lerna 7+

--- packages/@monorepo-utils/package-utils/test/get-packages-test.ts
+++ packages/@monorepo-utils/package-utils/test/get-packages-test.ts
@@ -13,95 +13,114 @@ describe("getPackages", () => {
     it("should match lerna.json", () => {
         const LERNA_PATH = path.join(__dirname, "fixtures/lerna");
         expect(getPackages(LERNA_PATH)).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "location": "<cwd>/test/fixtures/lerna/packages/bar",
-    "packageJSON": Object {
-      "name": "bar",
-    },
-  },
-  Object {
-    "location": "<cwd>/test/fixtures/lerna/packages/foo",
-    "packageJSON": Object {
-      "name": "foo",
-    },
-  },
-]
-`);
+            Array [
+              Object {
+                "location": "<cwd>/test/fixtures/lerna/packages/bar",
+                "packageJSON": Object {
+                  "name": "bar",
+                },
+              },
+              Object {
+                "location": "<cwd>/test/fixtures/lerna/packages/foo",
+                "packageJSON": Object {
+                  "name": "foo",
+                },
+              },
+            ]
+        `);
+    });
+    it("should match package.json rather than lerna.sjon", () => {
+        const LERNA_PATH = path.join(__dirname, "fixtures/lerna-and-packages");
+        expect(getPackages(LERNA_PATH)).toMatchInlineSnapshot(`
+            Array [
+              Object {
+                "location": "<cwd>/test/fixtures/lerna-and-packages/packages/bar",
+                "packageJSON": Object {
+                  "name": "bar",
+                },
+              },
+              Object {
+                "location": "<cwd>/test/fixtures/lerna-and-packages/packages/foo",
+                "packageJSON": Object {
+                  "name": "foo",
+                },
+              },
+            ]
+        `);
     });
     it("should match yarn-workspaces-old in package.json", () => {
         const YARN_OLD_PATH = path.join(__dirname, "fixtures/yarn-workspaces-old");
         expect(getPackages(YARN_OLD_PATH)).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "location": "<cwd>/test/fixtures/yarn-workspaces-old/packages/bar",
-    "packageJSON": Object {
-      "name": "bar",
-      "version": "0.0.0",
-    },
-  },
-  Object {
-    "location": "<cwd>/test/fixtures/yarn-workspaces-old/packages/foo",
-    "packageJSON": Object {
-      "name": "foo",
-      "version": "0.0.0",
-    },
-  },
-  Object {
-    "location": "<cwd>/test/fixtures/yarn-workspaces-old/package-2",
-    "packageJSON": Object {
-      "name": "package-2",
-      "version": "0.0.0",
-    },
-  },
-]
-`);
+            Array [
+              Object {
+                "location": "<cwd>/test/fixtures/yarn-workspaces-old/packages/bar",
+                "packageJSON": Object {
+                  "name": "bar",
+                  "version": "0.0.0",
+                },
+              },
+              Object {
+                "location": "<cwd>/test/fixtures/yarn-workspaces-old/packages/foo",
+                "packageJSON": Object {
+                  "name": "foo",
+                  "version": "0.0.0",
+                },
+              },
+              Object {
+                "location": "<cwd>/test/fixtures/yarn-workspaces-old/package-2",
+                "packageJSON": Object {
+                  "name": "package-2",
+                  "version": "0.0.0",
+                },
+              },
+            ]
+        `);
     });
     it("should match yarn-workspaces-new in package.json", () => {
         const YARN_NEW_PATH = path.join(__dirname, "fixtures/yarn-workspaces-new");
         expect(getPackages(YARN_NEW_PATH)).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "location": "<cwd>/test/fixtures/yarn-workspaces-new/packages/bar",
-    "packageJSON": Object {
-      "name": "bar",
-      "version": "0.0.0",
-    },
-  },
-  Object {
-    "location": "<cwd>/test/fixtures/yarn-workspaces-new/packages/foo",
-    "packageJSON": Object {
-      "name": "foo",
-      "version": "0.0.0",
-    },
-  },
-  Object {
-    "location": "<cwd>/test/fixtures/yarn-workspaces-new/package-2",
-    "packageJSON": Object {
-      "name": "package-2",
-      "version": "0.0.0",
-    },
-  },
-]
-`);
+            Array [
+              Object {
+                "location": "<cwd>/test/fixtures/yarn-workspaces-new/packages/bar",
+                "packageJSON": Object {
+                  "name": "bar",
+                  "version": "0.0.0",
+                },
+              },
+              Object {
+                "location": "<cwd>/test/fixtures/yarn-workspaces-new/packages/foo",
+                "packageJSON": Object {
+                  "name": "foo",
+                  "version": "0.0.0",
+                },
+              },
+              Object {
+                "location": "<cwd>/test/fixtures/yarn-workspaces-new/package-2",
+                "packageJSON": Object {
+                  "name": "package-2",
+                  "version": "0.0.0",
+                },
+              },
+            ]
+        `);
     });
     it("should support PNPM workspaces via the pnpm-workspace.yaml file", () => {
         const LERNA_PATH = path.join(__dirname, "fixtures/pnpm");
         expect(getPackages(LERNA_PATH)).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "location": "<cwd>/test/fixtures/pnpm/packages/bar",
-    "packageJSON": Object {
-      "name": "bar",
-    },
-  },
-  Object {
-    "location": "<cwd>/test/fixtures/pnpm/packages/foo",
-    "packageJSON": Object {
-      "name": "foo",
-    },
-  },
-]
-`);
+            Array [
+              Object {
+                "location": "<cwd>/test/fixtures/pnpm/packages/bar",
+                "packageJSON": Object {
+                  "name": "bar",
+                },
+              },
+              Object {
+                "location": "<cwd>/test/fixtures/pnpm/packages/foo",
+                "packageJSON": Object {
+                  "name": "foo",
+                },
+              },
+            ]
+        `);
     });
 });
